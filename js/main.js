18ffffe4bd6be16165342f6b4907d3bf4f5c1f8f
chris.torstenson <c.torsday+github@gmail.com>
2015-11-30 22:13:53 -0800

    version 1.1

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
+$(document).onLoad(function() {
+    // $('#portrait').click(function() {
+    //     var hidden = $('.hidden');
+    //     if (hidden.hasClass('visible')) {
+    //         hidden.animate({
+    //             "left": "-1000px"
+    //         }, "slow").removeClass('visible');
+    //     } else {
+    //         hidden.animate({
+    //             "left": "0px"
+    //         }, "slow").addClass('visible');
+    //     }
+    // });
 
+    // $('#portrait').animate(
+    //     { "left": "1000px" },
+    //     500
+    // );
+
+    $("#portrait").animate({'width': 240},500);
+});
